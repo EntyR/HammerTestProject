219e41d7aedde0ca5a929b081b411cd514171fe8
John Doe <ntr.reyzaxarov@gmail.com>
2021-09-02 13:13:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hammertestproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hammertestproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
